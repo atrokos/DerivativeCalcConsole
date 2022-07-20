bac70514b00f7a7a29d96983e6767117bb561600
atrokos <honza.pijalek@seznam.cz>
2022-07-20 10:12:01 +0200

    Changed if/else in Parser.TreeToPrefix to a switch block.

--- DerivativeCalcConsole/CSharpMath.cs
+++ DerivativeCalcConsole/CSharpMath.cs
@@ -34,6 +34,11 @@ namespace CSharpMath
                 Entity result = expr_string;
                 return result.Latexise();
             }
+            public void Simplify()
+            {
+                Entity simple = expr_string;
+                expr_string = simple.Simplify().ToString();
+            }
             public void Differentiate() // Thought: Further derivations can be stored in a List, so List[0] is OG func., List[1] is 1st der. etc.
             {
                 tree.Differentiate();
@@ -43,10 +48,11 @@ namespace CSharpMath
             {
                 return isDifferentiable;
             }
+
         }
         class Parser
         {
-            private static readonly HashSet<string> functions = new HashSet<string> { "sin", "cos", "tg", "tan", "cotg", "cotan", "abs", "arcsin", "arccos", "arctg", "arccotg", "ln", "sqrt" };
+            private static readonly HashSet<string> functions = new HashSet<string> { "sin", "cos", "tg", "tan", "cotg", "cotan", "abs", "arcsin", "arccos", "arctg", "arccotg", "ln", "sqrt", "cbrt" };
             private static readonly HashSet<string> operators = new HashSet<string> { "+", "-", "*", "/", "^" };
             private static readonly HashSet<string> variables = new HashSet<string> { "e", "pi" };
             private readonly string VAR;
@@ -66,6 +72,16 @@ namespace CSharpMath
                 string expression = PrefixToInfix(TreeToPrefix(tree));
                 pos = 0;
                 return expression;
+            }
+            public string ConvertToInfixHead(INode node)
+            {
+                Head head = new();
+                head.Add(node);
+
+                string expression = PrefixToInfix(TreeToPrefix(head));
+                Entity entity = expression;
+                pos = 0;
+                return entity.ToString();
             }
 
             List<string> ExprToList(string simplified)
@@ -275,89 +291,75 @@ namespace CSharpMath
                     }
                     else if (node is OPNode opnode)
                     {
-                        if (node is Plus)
+                        switch (opnode)
                         {
-                            result.Add("+");
-                            stack.Push(opnode.rightchild);
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Minus)
-                        {
-                            result.Add("-");
-                            stack.Push(opnode.rightchild);
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Multi)
-                        {
-                            result.Add("*");
-                            stack.Push(opnode.rightchild);
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Divi)
-                        {
-                            result.Add("/");
-                            stack.Push(opnode.rightchild);
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Power)
-                        {
-                            result.Add("^");
-                            stack.Push(opnode.rightchild);
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Sin)
-                        {
-                            result.Add("sin");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Cos)
-                        {
-                            result.Add("cos");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Tg)
-                        {
-                            result.Add("tan");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Cotg)
-                        {
-                            result.Add("cotan");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Arcsin)
-                        {
-                            result.Add("arcsin");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Arccos)
-                        {
-                            result.Add("arccos");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Arctg)
-                        {
-                            result.Add("arctan");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Arccotg)
-                        {
-                            result.Add("arccotan");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Abs)
-                        {
-                            result.Add("abs");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else if (node is Log)
-                        {
-                            result.Add("ln");
-                            stack.Push(opnode.leftchild);
-                        }
-                        else
-                        {
-                            throw new Exception($"TreeToPrefix: UNKNOWN OPTOKEN: {node}");
+                            case Plus:
+                                result.Add("+");
+                                stack.Push(opnode.rightchild);
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Minus:
+                                result.Add("-");
+                                stack.Push(opnode.rightchild);
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Multi:
+                                result.Add("*");
+                                stack.Push(opnode.rightchild);
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Divi:
+                                result.Add("/");
+                                stack.Push(opnode.rightchild);
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Power:
+                                result.Add("^");
+                                stack.Push(opnode.rightchild);
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Sin:
+                                result.Add("sin");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Cos:
+                                result.Add("cos");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Tg:
+                                result.Add("tan");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Cotg:
+                                result.Add("cotan");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Arcsin:
+                                result.Add("arcsin");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Arccos:
+                                result.Add("arccos");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Arctg:
+                                result.Add("arctan");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Arccotg:
+                                result.Add("arccotan");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Abs:
+                                result.Add("abs");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            case Log:
+                                result.Add("ln");
+                                stack.Push(opnode.leftchild);
+                                break;
+                            default:
+                                throw new Exception($"TreeToPrefix: UNKNOWN OPTOKEN: {node}");
                         }
                     }
                 }
@@ -538,6 +540,18 @@ namespace CSharpMath
 
                             node = power1;
                             break;
+                        case "cbrt":
+                            Power power2 = new();
+                            pos++;
+                            power2.Add(BuildTree(expr));
+
+                            Divi cbrtdivi = new();
+                            Constant one2 = new(1), three = new(3);
+                            cbrtdivi.SetChildren(one2, three);
+                            power2.Add(cbrtdivi);
+
+                            node = power2;
+                            break;
                         default:
                             throw new Exception("UNKNOWN OPTOKEN");
                     }
@@ -581,6 +595,25 @@ namespace CSharpMath
                         if (operators.Contains(oper))
                             return 2;
                         return 0;
+                }
+            }
+        }
+
+        public class StepGenerator
+        {
+            private Parser parser = new("x");
+            private static readonly Dictionary<string, string> descriptions = new() { { "+", "Plus se derivuje jednoduše: zderivujeme levou i pravou stranu, přičemž sčítání ponecháme." } };
+            public string Generate()
+            {
+                foreach (List<INode> nodes in Storage.Steps)
+                {
+                    Console.WriteLine(parser.ConvertToInfixHead(nodes[0]) + " se zderivuje na:");
+                    switch (nodes[0])
+                    {
+                        case Plus:
+
+                            break;
+                    }
                 }
             }
         }
